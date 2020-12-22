1b878be674e7f268aeb0a6e1f55908844e8c27d5
Heesun <heesunmelody@gmail.com>
2020-12-22 17:07:35 +0900

    bottom navigation - fragment

---
+++ app/src/main/java/com/example/piggybank/CompareFragment.java
@@ -0,0 +1,22 @@
+package com.example.piggybank;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CompareFragment extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_compare, container, false);
+
+        return view;
+    }
+
+}
+
